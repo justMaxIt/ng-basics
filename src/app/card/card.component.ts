9b675d6c4fd3abbfdabf9118f8f1264e2b348c4b
Maksym Fedorenko <justmaksit@gmail.com>
2021-05-26 10:27:19 +0300

    data to card component, examples

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -3,7 +3,21 @@ import { Component } from '@angular/core';
 @Component({
    selector: 'app-card',
    templateUrl: './card.component.html',
-   styleUrls: ['./card.component.scss']
+   styleUrls: ['./card.component.scss'],
+   interpolation: ['{{', '}}']
 })
 
-export class CardComponent { }
+export class CardComponent {
+   title = 'My Card Title'
+   text = 'My text'
+   number = 42
+   array = [1, 1, 2, 3, 5, 8]
+   obj = {
+      name: 'Maks',
+      info: {
+         age: 34,
+         job: 'Frontend'
+      }
+   }
+
+}
