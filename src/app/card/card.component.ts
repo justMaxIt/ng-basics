e9b6e828aab07d6546cf5c737b1e0e4964c1a8ea
Maksym Fedorenko <justmaksit@gmail.com>
2021-05-27 23:41:56 +0300

    added button changeTitle on main branch and deleted others examples

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -10,20 +10,10 @@ import { Component, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
    title: string = 'My Card Title'
    text: string = 'My text'
-   imgUrl: string = 'https://spng.subpng.com/20180329/jue/kisspng-angularjs-dart-front-and-back-ends-npm-escalator-5abda7d6ba62f5.4348649815223787107634.jpg'
-   disabled: boolean = false
 
-   // number = 42
-   // array = [1, 1, 2, 3, 5, 8]
-   // obj = {name: 'Maks', info: {age: 34,job: 'Frontend'}}
-   ngOnInit() {
-      setTimeout(() => {
-         this.imgUrl = 'https://www.dotcom-monitor.com/blog/wp-content/uploads/sites/3/2020/05/Vue-logo-1.png';
-         this.disabled = true
-      }, 2000)
-   }
-   getInfo(): string {
-      return "This is my info"
+   ngOnInit() { }
+   changeTitle() {
+      this.title = "This title has been changed"
    }
 
 }
